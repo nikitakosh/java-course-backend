602eda5a8d082de6bfb3f6eff4d4ed090884f956
nikitakosh <kosheevnikita@gmail.com>
2024-02-19 23:34:33 +0300

    change delay on interval

--- scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
@@ -3,14 +3,12 @@ package edu.java;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
 @EnableScheduling
 public class LinkUpdaterScheduler {
 
-    @Scheduled(fixedDelayString = "#{@scheduler.forceCheckDelay()}")
+    @Scheduled(fixedDelayString = "#{@scheduler.interval()}")
     public void update() {
         log.info("link was updated!");
     }
